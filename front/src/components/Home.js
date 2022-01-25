a49f60540f60dbd971ab3a609275baffed66a2cb
Jeong Hyunji <guswl3201@naver.com>
2022-01-25 17:15:19 +0900

    first page

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Personal from './Personal';
 
 function Home(){
     return (
         <div>
+            <Personal id="0"></Personal>
+            <Personal id="1"></Personal>
+            <Personal id="2"></Personal>
 
         </div>
     );
---
+++ front/src/components/Personal.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import Lee from "../imgs/lee.png";
+import Ahn from "../imgs/an.png";
+import Yoon from "../imgs/yoon.png";
+
+
+function Personal(props){
+    const id = Number(props.id);
+    const candidate_party = ['더불어 민주당', '국민의 힘', '국민의 당'];
+    const candidate_img = [Lee, Yoon, Ahn];
+    const candidate_name = ["이재명",'윤석열','안철수'];
+    const candidate_promise1 = ["· 기본 시리즈 정책 ",'· 여성 가족부 폐지','· 과학기술 중심 국가 건설'];
+    const candidate_promise2 = ["· 스마트 강군 건설",'· 민간 주도 일자리 창출','· 정신 건강 국가 책임제'];
+    const candidate_promise3 = ["· 세계 5강 경제대국 달성 ",'· 조세 인하 ','· G5 경제강국 건설'];
+    const  [Inputs, setInputs] = useState({
+        party: candidate_party[id],
+        img: candidate_img[id],
+        name: candidate_name[id],
+        promise1 : candidate_promise1[id],
+        promise2 : candidate_promise2[id],
+        promise3 : candidate_promise3[id]
+    });
+
+    return (
+        <div>
+            <div>
+                <h2>{Inputs.party}</h2>
+                <img src={Inputs.img}/>
+                <h3>{Inputs.name}</h3>
+                <p>{Inputs.promise1}<br/>{Inputs.promise2}<br/>{Inputs.promise3}</p>
+                <button> ▶ 자세히 보러가기</button>
+
+            </div>
+        </div>
+    );
+}
+
+export default Personal;
