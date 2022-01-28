ccd820eecfc7b7cce27fd1a868f522dd15184853
Jeong Hyunji <guswl3201@naver.com>
2022-01-28 16:03:37 +0900

    promise finish

---
+++ front/src/components/Command.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Command(){
+    return(
+        <div>
+            댓글을 입력하세여
+            <input type="text" value="commad"></input>
+        </div>
+    );
+
+}
+
+
+export default Command;
--- front/src/components/Total.js
+++ front/src/components/Total.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CanButton from "./Can_button";
 import HomeButton from "./HomeButton";
+import Command from "./Command";
 import Pye from "./Pye";
 import PartyWordCloud
  from './PartyWordColud';
@@ -11,7 +12,7 @@ function Total(){
             <Pye title="▶ 나이 별 관심분야" src="http://ec2-15-164-49-201.ap-northeast-2.compute.amazonaws.com:8080/whole_sex/"></Pye>
             <Pye title="▶ 성별/나이 별 관심 분야" src='http://ec2-15-164-49-201.ap-northeast-2.compute.amazonaws.com:8080/whole_sex/'></Pye>
             <PartyWordCloud></PartyWordCloud>
-            <div style={{position:"relative",clear:"both"}}> 
+            <div style={{position:"relative",clear:"both"}}> {/*  <Command> </Command>*/}
             <CanButton can_id ="0"></CanButton>
             <CanButton  can_id ="1"></CanButton>
             <CanButton  can_id ="2"></CanButton>
