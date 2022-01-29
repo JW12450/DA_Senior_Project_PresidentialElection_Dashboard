f834615ea543b23f2b883942f979f70569e9c994
Jeong Hyunji <guswl3201@naver.com>
2022-01-29 11:16:51 +0900

    f

--- front/src/components/Command.js
+++ front/src/components/Command.js
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from "react";
 import "../css/command.css";
 import "../font/font.css";import "./TypeResult";
 import TypeResult from "./TypeResult";
+import axios from 'axios';
 
 function Command(){
 
@@ -15,14 +16,23 @@ function Command(){
     function Reset(e){
         setText("");
         setLoad(false);
+        setIndex(null);
     }
-
+    const [Index, setIndex] = useState(null);
     const sendcommand=()=>{
 
         setLoad(true);
         console.log(text)
         //여기서 보내고 받아오기 다 하기, props로 type result에 넘겨주기
-
+        axios.post("https://fbbd3ce4-ec2a-48fe-8483-17916fb08db7.mock.pstmn.io/postparty",{text},{
+            headers: { "Content-Type": `application/json`}
+            })
+        .then((res)=>console.log(res.config.data))
+        .catch((err)=> console.log(err))
+        
+        axios.get("https://52407797-85c0-49f5-b5ac-08d53268f097.mock.pstmn.io/party")
+        .then((res)=> {setIndex(res.data.id)})
+        .catch((err)=>{console.log(err)})
     }
 
     return(
@@ -32,7 +42,7 @@ function Command(){
             <textarea value={text} onChange={InputText} placeHolder="이번 선거에 대한 당신의 전반적인 견해를 입력해주세요" className="commandbox"></textarea></div>
             <input type="button" value="다시 입력하기" onClick={Reset} className="resetbtn" style={{fontFamily:"Cafe24Oneprettynight"}}></input>
             <input type="button" value="나의 정치성향 알아보기" onClick={sendcommand} className="enter_btn" style={{fontFamily:"Cafe24Oneprettynight"}}></input>
-            <TypeResult bool={load}> </TypeResult>
+            <TypeResult bool={load} id={Index}> </TypeResult>
         </div>
     );
 
--- front/src/components/TypeResult.js
+++ front/src/components/TypeResult.js
@@ -3,9 +3,9 @@ import data from "../data";
 import "../css/command.css";
 
 function TypeResult(props){
+    var index = Number(props.id);
 
     var [loading, setLoading] = useState(false);
-
 
     useEffect(() => {
         if (props.bool === true) {
@@ -16,12 +16,20 @@ function TypeResult(props){
       }, [props.bool])
 
 
+
     var [showResult, setShowResult] = useState(false);
+
+
+    useEffect(() => {
+        if(props.id != null){
+        setLoading(false);
+        setShowResult(true);} 
+      }, [props.id])
 
     return(
         <div>
             {loading === true ? <p className='answer'>잠시만 기다려주세요</p>: null}
- 
+            {showResult === true ? <p className='answer'> 당신의 성향은 {data[index].type} 입니다</p>:null}
         </div>
 
     )
